1ceca8feadd9f51fdc4ccd4f3e429bb45d9ee923
BoJoNVi <bonjomontes@gmail.com>
2023-04-24 18:01:18 +0800

    feat: implemented logout feature

--- src/application/layouts/HomeLayout.tsx
+++ src/application/layouts/HomeLayout.tsx
@@ -1,15 +1,40 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { authLogout } from "../../domain/service/authService";
 
 function HomeLayout() {
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    authLogout();
+
+    navigate("/");
+  }
+
   return (
     <div className="min-h-screen flex flex-col">
       <header className="bg-blue-300 border-b-2 p-2 flex items-center border-blue-900">
-        <h2 className="text-lg font-medium">Navbar Logo</h2>
+        {/* Left */}
+        <div className="flex items-center space-x-2">
+          <div className="w-32">
+            <div className="relative pb-[40%] bg-white">
+              <p className="absolute">Logo Here</p>
+            </div>
+          </div>
+          <h1 className="text-xl font-medium">Davao Parking Dashboard</h1>
+        </div>
+
+        {/* Right */}
         <div className="ml-auto flex items-center space-x-2">
-          <div className="w-10 h-10 rounded-full border border-blue-900"></div>
+          <div className="w-10 h-10 rounded-full border border-blue-900 bg-white"></div>
           <p>Admin</p>
         </div>
+        <button
+          onClick={handleLogout}
+          className="ml-4 py-1 px-4 bg-red-500 text-white rounded-md transition hover:opacity-90"
+        >
+          Logout
+        </button>
       </header>
       <div className="flex flex-1">
         <div className="w-[300px] bg-blue-300 border-r-2 border-blue-900">
--- src/domain/service/authService.ts
+++ src/domain/service/authService.ts
@@ -2,6 +2,8 @@ export async function authLogin(values: Record<string, any>) {
   const { username, password } = values;
 
   if (username === "admin" && password === "admin") {
+    document.cookie = "token=123; path=/; max-age=3600";
+
     return {
       username: "admin",
     };
@@ -12,4 +14,8 @@ export async function authLogin(values: Record<string, any>) {
       message: "Invalid username or password",
     },
   };
+}
+
+export async function authLogout() {
+  document.cookie = "token=; path=/; max-age=0";
 }
