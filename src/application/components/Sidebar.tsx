1a788801a549194941a1e66f7ea3a7c9a87295f5
BoJoNVi <bonjomontes@gmail.com>
2023-04-25 13:21:22 +0800

    improvements: polished the sidebar behavior

--- src/application/components/Sidebar.tsx
+++ src/application/components/Sidebar.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { Icon } from "@iconify/react";
 
-function Sidebar() {
+interface Props {
+  open: boolean;
+}
+
+function Sidebar({ open }: Props) {
   const navigations = [
     { label: "Home", icon: "mdi:home", path: "/" },
     { label: "Users", icon: "mdi:account-group", path: "/users" },
@@ -10,27 +14,42 @@ function Sidebar() {
   ];
 
   return (
-    <ul>
-      {navigations.map((navigation) => {
-        return (
-          <li className="p-0.5" key={navigation.path}>
-            <NavLink
-              className={({ isActive }) =>
-                `p-2 rounded-md flex items-center ${
-                  isActive ? "bg-blue-100" : "hover:bg-blue-100"
-                } `
-              }
-              to={navigation.path}
-            >
-              <span className="inline-block w-6">
-                <Icon icon={navigation.icon} />
-              </span>
-              {navigation.label}
-            </NavLink>
-          </li>
-        );
-      })}
-    </ul>
+    <aside
+      className={`sticky top-[66px] h-[calc(100vh-66px)] bg-blue-300 border-r-2 border-blue-900 ${
+        open ? "min-w-[200px]" : "min-w-[1px]"
+      } transition-all ease-linear group hover:min-w-[200px]`}
+    >
+      {/* Navigation */}
+      <nav className="p-2 mt-4">
+        <ul>
+          {navigations.map((navigation) => {
+            return (
+              <li className="p-0.5" key={navigation.path}>
+                <NavLink
+                  className={({ isActive }) =>
+                    `p-2 rounded-md flex items-center ${
+                      isActive ? "bg-blue-100" : "hover:bg-blue-100"
+                    } `
+                  }
+                  to={navigation.path}
+                >
+                  <span>
+                    <Icon className="w-6 h-6" icon={navigation.icon} />
+                  </span>
+                  <span
+                    className={`transition-all ease-linear overflow-hidden ${
+                      open ? "max-w-[100px] px-2" : "max-w-[0]"
+                    } group-hover:max-w-[100px] group-hover:px-2`}
+                  >
+                    {navigation.label}
+                  </span>
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </aside>
   );
 }
 
--- src/application/layouts/HomeLayout.tsx
+++ src/application/layouts/HomeLayout.tsx
@@ -7,6 +7,7 @@ import NavbarProfileMenu from "../components/navbar/NavbarProfileMenu";
 import { Icon } from "@iconify/react";
 
 function HomeLayout() {
+  const [openSidebar, setOpenSidebar] = React.useState(true);
   const [openProfileMenu, setProfileMenu] = React.useState(false);
   const navigate = useNavigate();
 
@@ -17,48 +18,56 @@ function HomeLayout() {
   }, [navigate]);
 
   return (
-    <div className="flex bg-blue-50">
-      {/* Sidebar */}
-      <aside className="w-[300px] h-screen sticky top-0 bg-blue-300 border-r-2 border-blue-900">
-        {/* Sidebar header */}
-        <header className="p-2">
+    <div className="flex min-h-screen flex-col bg-blue-50">
+      {/* Navbar */}
+      <header className="bg-blue-300 sticky top-0 border-b-2 py-2 px-4 flex items-center border-blue-900">
+        {/* Left side */}
+        <div className="flex space-x-4 items-center">
+          {/* Sidebar show and hide btn */}
+          <button>
+            <Icon
+              className="w-8 h-8"
+              icon={openSidebar ? "mdi:menu-open" : "mdi:menu"}
+              onClick={() => setOpenSidebar(!openSidebar)}
+            />
+          </button>
+
+          {/* Logo */}
           <div className="flex items-center space-x-2">
-            <div className="w-32">
-              <div className="relative pb-[40%] bg-white">
+            <div className="w-12">
+              <div className="relative pb-[100%] rounded-full overflow-hidden bg-white">
                 <p className="absolute">Davao Parking logo</p>
               </div>
             </div>
+            <h1 className="text-2xl font-bold">Parking Admin</h1>
           </div>
-        </header>
+        </div>
 
-        {/* Navigation */}
-        <nav className="p-2 mt-4">
-          <Sidebar />
-        </nav>
-      </aside>
+        {/* Right side */}
+        <div className="ml-auto flex items-center space-x-2">
+          <div className="w-10 h-10 rounded-full border border-blue-900 bg-white"></div>
+          <p>Admin</p>
+          <Popover
+            open={openProfileMenu}
+            content={<NavbarProfileMenu setProfileMenu={setProfileMenu} />}
+            placement="topRight"
+            trigger="click"
+            onOpenChange={(open) => setProfileMenu(open)}
+            zIndex={1001}
+          >
+            <button
+              className={`transition ${openProfileMenu ? "rotate-180" : ""}`}
+            >
+              <Icon className="w-8 h-8" icon="mdi:caret-down" />
+            </button>
+          </Popover>
+        </div>
+      </header>
 
       {/* Right Content */}
-      <main className="flex-1">
-        <header className="bg-blue-300 sticky top-0 border-b-2 p-2 flex items-center border-blue-900">
-          <div className="ml-auto flex items-center space-x-2">
-            <div className="w-10 h-10 rounded-full border border-blue-900 bg-white"></div>
-            <p>Admin</p>
-            <Popover
-              open={openProfileMenu}
-              content={<NavbarProfileMenu setProfileMenu={setProfileMenu} />}
-              placement="topRight"
-              trigger="click"
-              onOpenChange={(open) => setProfileMenu(open)}
-              zIndex={1001}
-            >
-              <button
-                className={`transition ${openProfileMenu ? "rotate-180" : ""}`}
-              >
-                <Icon className="w-8 h-8" icon="mdi:caret-down" />
-              </button>
-            </Popover>
-          </div>
-        </header>
+      <main className="flex-1 flex">
+        {/* Sidebar */}
+        <Sidebar open={openSidebar} />
         <div className="flex flex-1">
           <Outlet />
         </div>
