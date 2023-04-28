932d2e4bc33b41bbb610d48cae46cc3d347d069b
BoJoNVi <bonjomontes@gmail.com>
2023-04-28 13:53:40 +0800

    feat: implemented custom hooks for graphql

--- src/application/query/GraphqlProvider.tsx
+++ src/application/query/GraphqlProvider.tsx
@@ -3,7 +3,8 @@ import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
-  gql,
+  useQuery,
+  DocumentNode,
 } from "@apollo/client";
 
 interface Props {
@@ -11,9 +12,26 @@ interface Props {
 }
 
 const client = new ApolloClient({
-  uri: "http://192.168.1.11:5180/graphql/",
+  uri: "http://192.168.1.11:5180/graphql",
   cache: new InMemoryCache(),
 });
+
+export function graphqlQuery(schema: DocumentNode, name: string) {
+  const query = useQuery(schema);
+
+  React.useEffect(() => {
+    if (query.data) {
+      console.log(`${name} debug`);
+      console.log({
+        data: query.data,
+        errors: query.error,
+        loading: query.loading,
+      });
+    }
+  }, [query.data]);
+
+  return query;
+}
 
 function GraphqlProvider({ children }: Props) {
   return <ApolloProvider client={client}>{children}</ApolloProvider>;
