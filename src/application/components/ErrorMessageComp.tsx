4dfe27484af83a2020a2e26f004cdb7a8d2b142e
BoJoNVi <bonjomontes@gmail.com>
2023-05-03 14:35:30 +0800

    fix: problem incorrect z-index of each elements

--- src/application/components/ErrorMessageComp.tsx
+++ src/application/components/ErrorMessageComp.tsx
@@ -1,12 +1,10 @@
-import React from "react";
-
 interface Props {
   message?: string;
 }
 
 function ErrorMessageComp({ message }: Props) {
   return (
-    <div className="absolute top-0 left-0 w-full h-full flex bg-black bg-opacity-50 z-[2000] items-center justify-center">
+    <div className="absolute top-0 left-0 w-full h-full flex bg-black bg-opacity-50 z-[9] items-center justify-center">
       <div className="w-1/2 text-center p-16 bg-white rounded-lg">
         <header>
           <h1>Oh no! Something wrong happened.</h1>
