385f2b2cf71ab3a79c647f77910af3b476567f62
BoJoNVi <bonjomontes@gmail.com>
2023-05-31 17:08:00 +0800

    refactor: revamp the select async component

---
+++ src/application/components/select/SelectAsync.tsx
@@ -0,0 +1,45 @@
+import { Select, message } from "antd";
+import React from "react";
+
+interface Props {
+  className?: string;
+  getData: Function;
+  placeholder?: string;
+  mode?: "multiple" | "tags";
+}
+
+function SelectAsync({ className, getData, placeholder, mode }: Props) {
+  const [data, setData] = React.useState<any>([]);
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    async function fetchData() {
+      const response = await getData();
+
+      if (response.errors) {
+        message.error("Failed to fetch data.");
+      } else {
+        setData(response.data);
+      }
+
+      setLoading(false);
+    }
+
+    if (loading) {
+      fetchData();
+    }
+  }, []);
+
+  return (
+    <Select
+      className={`w-full ${className}`}
+      loading={loading}
+      disabled={loading}
+      options={data}
+      placeholder={placeholder}
+      mode={mode}
+    />
+  );
+}
+
+export default SelectAsync;
