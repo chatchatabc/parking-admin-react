809b7bb69eaee58101d3d741f4dc6c163a9633e6
BoJoNVi <bonjomontes@gmail.com>
2023-06-01 14:23:33 +0800

    feat: finalized the inputs for creating parking lot

--- src/application/components/forms/FormParkingCreate.tsx
+++ src/application/components/forms/FormParkingCreate.tsx
@@ -1,7 +1,18 @@
-import { Button, Form, FormInstance, Input } from "antd";
+import {
+  Button,
+  Checkbox,
+  Form,
+  FormInstance,
+  Input,
+  TimePicker,
+  message,
+} from "antd";
+import type { Dayjs } from "dayjs";
 import { userAllOptionsGet } from "../../../domain/services/userService";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import SelectAsyncSearch from "../select/SelectAsyncSearch";
+import { parkingCreate } from "../../../domain/services/parkingService";
+import { drawerFormUpdate } from "../../redux/slices/drawers/drawerForm";
 
 type Props = {
   title: string;
@@ -9,73 +20,193 @@ type Props = {
 };
 
 function FormParkingCreate({ title, formRef }: Props) {
+  const dispatch = useDispatch();
   const drawerForm = useSelector((state: any) => state.drawerForm);
 
   async function onFinish(e: any) {
+    const openDaysFlag = e.openDaysFlag as number[];
+    const businessHoursEnd = e.businessHoursEnd as Dayjs;
+    const businessHoursStart = e.businessHoursStart as Dayjs;
+
+    e.businessHoursEnd = businessHoursEnd.format("YYYY-MM-DD HH:mm:ss.SSS");
+    e.businessHoursStart = businessHoursStart.format("YYYY-MM-DD HH:mm:ss.SSS");
+
+    e.openDaysFlag = openDaysFlag.reduce((acc, curr) => {
+      return acc | curr;
+    }, 0);
+
     console.log(e);
+
+    const response = await parkingCreate(e);
+
+    if (response.errors) {
+      return message.error("Parking lot creation failed");
+    }
+
+    message.success("Parking lot created successfully");
+    formRef.resetFields();
+
+    dispatch(
+      drawerFormUpdate({
+        show: false,
+      })
+    );
   }
 
   return (
     <Form name={title} onFinish={onFinish} layout="vertical" form={formRef}>
       <div className="flex flex-wrap [&>*]:px-2 [&>*]:w-1/3">
-        <Form.Item name="userUuid" hidden />
-        <Form.Item name="username" label="Username">
-          <Input placeholder="Username" />
-        </Form.Item>
-
         <Form.Item
-          name="phone"
-          label="Phone Number"
-          rules={[
-            {
-              message: "Need some input",
-              required: true,
-            },
-            {
-              pattern: new RegExp(/^[0-9\b]+$/),
-              message: "Invalid phone number",
-            },
-          ]}
-        >
-          <Input placeholder="09123456789" />
-        </Form.Item>
-
-        <Form.Item
-          name="email"
-          label="Email"
-          rules={[
-            {
-              type: "email",
-              message: "Invalid email",
-            },
-          ]}
-        >
-          <Input placeholder="Email" />
-        </Form.Item>
-
-        <Form.Item
-          name="roles"
-          label="Roles"
+          name="userUuid"
+          label="Username"
           rules={[
             {
               message: "Need some input",
               required: true,
             },
           ]}
-          shouldUpdate
         >
           {SelectAsyncSearch({
             placeholder: "Roles",
             getData: userAllOptionsGet,
-            mode: "multiple",
           })}
         </Form.Item>
+        <Form.Item
+          style={{ width: "66%" }}
+          name="name"
+          label="Parking Lot Name"
+          rules={[
+            {
+              message: "Need some input",
+              required: true,
+            },
+          ]}
+        >
+          <Input placeholder="Parking Lot Name" />
+        </Form.Item>
 
-        <Form.Item hidden className="w-full">
+        <Form.Item
+          style={{ width: "50%" }}
+          name="latitude"
+          label="Latitude"
+          rules={[
+            {
+              message: "Need some input",
+              required: true,
+            },
+            {
+              pattern: new RegExp(/^[0-9]+(\.[0-9]+)?$/),
+              message: "Invalid Latitude",
+            },
+          ]}
+        >
+          <Input placeholder="Latitude" />
+        </Form.Item>
+
+        <Form.Item
+          style={{ width: "50%" }}
+          name="longitude"
+          label="Longitude"
+          rules={[
+            {
+              message: "Need some input",
+              required: true,
+            },
+            {
+              pattern: new RegExp(/^[0-9]+(\.[0-9]+)?$/),
+              message: "Invalid Longitude",
+            },
+          ]}
+        >
+          <Input placeholder="Longitude" />
+        </Form.Item>
+
+        <Form.Item
+          style={{ width: "100%" }}
+          name="address"
+          label="Address"
+          rules={[
+            {
+              message: "Need some input",
+              required: true,
+            },
+          ]}
+        >
+          <Input placeholder="Address" />
+        </Form.Item>
+
+        <Form.Item
+          name="capacity"
+          label="Total Capacity"
+          rules={[
+            {
+              message: "Need some input",
+              required: true,
+            },
+            {
+              pattern: new RegExp(/^[0-9]+$/),
+              message: "Invalid Capacity",
+            },
+          ]}
+        >
+          <Input placeholder="Total Capacity" />
+        </Form.Item>
+
+        <Form.Item
+          name="businessHoursStart"
+          label="Starting Business Hours"
+          rules={[
+            {
+              message: "Need some input",
+              required: true,
+            },
+          ]}
+        >
+          {/* Format HH:mm AM/PM */}
+          <TimePicker className="w-full" format={"HH:mm a"} />
+        </Form.Item>
+
+        <Form.Item
+          name="businessHoursEnd"
+          label="Ending Business Hours"
+          rules={[
+            {
+              message: "Need some input",
+              required: true,
+            },
+          ]}
+        >
+          {/* Format HH:mm AM/PM */}
+          <TimePicker className="w-full" format={"HH:mm a"} />
+        </Form.Item>
+
+        <Form.Item
+          style={{ width: "100%" }}
+          name="openDaysFlag"
+          label="Open Days"
+          rules={[
+            {
+              message: "Need some input",
+              required: true,
+            },
+          ]}
+        >
+          <Checkbox.Group className="w-full flex">
+            <Checkbox value={1}>Sunday</Checkbox>
+            <Checkbox value={2}>Monday</Checkbox>
+            <Checkbox value={4}>Tuesday</Checkbox>
+            <Checkbox value={8}>Wednesday</Checkbox>
+            <Checkbox value={16}>Thursday</Checkbox>
+            <Checkbox value={32}>Friday</Checkbox>
+            <Checkbox value={64}>Saturday</Checkbox>
+          </Checkbox.Group>
+        </Form.Item>
+
+        <Form.Item style={{ width: "100%" }}>
           <Button
             loading={drawerForm.loading}
             htmlType="submit"
-            className="bg-primary text-white"
+            className="bg-primary text-white w-full block"
           >
             Submit
           </Button>
