e78b1a54e8f56ea286ac2f8e6af85c86aeca8a69
BoJoNVi <bonjomontes@gmail.com>
2023-06-02 14:53:53 +0800

    feat: polished the search page tables

--- src/application/pages/SearchPage.tsx
+++ src/application/pages/SearchPage.tsx
@@ -4,8 +4,23 @@ import ParkingTable from "../components/tables/ParkingLotTable";
 function SearchPage() {
   return (
     <div className="px-4 pb-4 space-y-4 w-full relative">
-      <UsersTable showPagination={false} />
-      <ParkingTable showPagination={false} />
+      <section className="p-4 border-2 rounded-lg bg-bg1 border-primary">
+        <header>
+          <h2 className="text-xl font-bold">Users Table</h2>
+        </header>
+        <section className="mt-2">
+          <UsersTable showPagination={false} />
+        </section>
+      </section>
+
+      <section className="p-4 border-2 rounded-lg bg-bg1 border-primary">
+        <header>
+          <h2 className="text-xl font-bold">Parking Lots Table</h2>
+        </header>
+        <section className="mt-2">
+          <ParkingTable showPagination={false} />
+        </section>
+      </section>
     </div>
   );
 }
