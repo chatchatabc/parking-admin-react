0db53b4ae3ea54d797cbbd84024c646b1fa64b5c
BoJoNVi <bonjomontes@gmail.com>
2023-06-02 10:19:36 +0800

    refactor: removed unnecessary codes for parking page

--- src/application/pages/parking/ParkingPage.tsx
+++ src/application/pages/parking/ParkingPage.tsx
@@ -1,38 +1,11 @@
-import React from "react";
-import { Pagination } from "antd";
 import { formRefHandler } from "../../layouts/HomeLayout";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { drawerFormUpdate } from "../../redux/slices/drawers/drawerForm";
-import { useSearchParams } from "react-router-dom";
-import { Parking } from "../../../domain/models/ParkingModel";
-import { User } from "../../../domain/models/UserModel";
 import ParkingTable from "../../components/tables/ParkingTable";
 
 function ParkingPage() {
   // React Router
   const dispatch = useDispatch();
-  const [searchParams, _] = useSearchParams();
-
-  // Saved States
-  const globalState = useSelector((state: any) => state.globalState);
-  const keyword = searchParams.get("keyword") ?? undefined;
-  const page = searchParams.get("page") ?? 1;
-  const pageSize = searchParams.get("pageSize") ?? 10;
-
-  // Local States
-  const [loading, setLoading] = React.useState(true);
-  const [pagination, setPagination] = React.useState({
-    current: Number(page) - 1,
-    pageSize: Number(pageSize),
-    total: 0,
-  });
-  const [data, setData] = React.useState<
-    (Parking & { owner: User; key: string })[]
-  >([]);
-
-  function handleNavigation(page: number) {
-    setPagination({ ...pagination, current: page - 1 });
-  }
 
   return (
     <div className="px-4 pb-4 w-full relative">
@@ -44,13 +17,7 @@ function ParkingPage() {
         </header>
 
         {/* Table Actions */}
-        <section className="flex gap-2">
-          <Pagination
-            current={pagination.current + 1}
-            total={pagination.total}
-            onChange={handleNavigation}
-          />
-
+        <section className="flex justify-end gap-2">
           <button
             onClick={() => {
               formRefHandler.resetFields();
