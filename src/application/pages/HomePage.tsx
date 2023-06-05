77d733a3993eb91fa808fc9405cd0a3c3f32d983
BoJoNVi <bonjomontes@gmail.com>
2023-06-05 09:40:24 +0800

    refactor: changed the theme of homepage

--- src/application/pages/HomePage.tsx
+++ src/application/pages/HomePage.tsx
@@ -1,34 +1,39 @@
-import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import ParkingTable from "../components/tables/ParkingLotTable";
 import UsersTable from "../components/tables/UsersTable";
+import MyButton from "../components/common/MyButton";
 
 function HomePage() {
+  const navigate = useNavigate();
+
   return (
-    <div className="space-y-8 p-4 bg-bg1 [&>*]:p-2">
-      <section className="w-full bg-bg1 rounded-lg">
+    <div className="space-y-8 p-4 bg-bg1">
+      <section className="w-full bg-bg2 p-4 rounded-lg">
         <header className="flex justify-between">
           <h2 className="text-xl font-bold">Parking Lots Table</h2>
-          <NavLink
-            className="bg-c1 ml-auto text-white px-4 py-1 rounded-md transition hover:bg-blue-600"
-            to={"/parking-lots"}
+          <MyButton
+            onClick={() => {
+              navigate("/parking-lots");
+            }}
           >
             Open
-          </NavLink>
+          </MyButton>
         </header>
         <section className="mt-2">
           <ParkingTable showPagination={false} />
         </section>
       </section>
 
-      <section className="w-full bg-bg1 rounded-lg">
+      <section className="w-full bg-bg2 p-4 rounded-lg">
         <header className="flex justify-between">
           <h2 className="text-xl font-bold">Users Table</h2>
-          <NavLink
-            className="bg-c1 ml-auto text-white px-4 py-1 rounded-md transition hover:bg-blue-600"
-            to={"/users"}
+          <MyButton
+            onClick={() => {
+              navigate("/users");
+            }}
           >
             Open
-          </NavLink>
+          </MyButton>
         </header>
         <section className="mt-2">
           <UsersTable showPagination={false} />
