05ab7888f238c8ebe1749fe38914603019bdfdc4
BoJoNVi <bonjomontes@gmail.com>
2023-06-06 13:39:00 +0800

    feat: implemented dynamic donut chart

---
+++ src/application/components/donut-charts/DynamicDonut.tsx
@@ -0,0 +1,30 @@
+import { Spin } from "antd";
+import Chart from "react-apexcharts";
+
+type Props = {
+  series: number[];
+  labels: string[];
+  loading: boolean;
+};
+
+function DynamicDonut({ series, labels, loading }: Props) {
+  return (
+    <div className="relative">
+      {loading && (
+        <div className="z-[1] absolute h-full w-full bg-white bg-opacity-75 flex items-center justify-center">
+          <Spin />
+        </div>
+      )}
+      <Chart
+        series={series}
+        options={{
+          labels,
+        }}
+        type="donut"
+        width="100%"
+      />
+    </div>
+  );
+}
+
+export default DynamicDonut;
