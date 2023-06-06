c77e492365a201925fa1b7721a32beaaf544d1af
BoJoNVi <bonjomontes@gmail.com>
2023-06-06 13:51:01 +0800

    feat: added donut graphs to the home page

--- src/application/pages/HomePage.tsx
+++ src/application/pages/HomePage.tsx
@@ -1,42 +1,103 @@
 import { useNavigate } from "react-router-dom";
-import ParkingTable from "../components/tables/ParkingLotTable";
 import UsersTable from "../components/tables/UsersTable";
 import MyButton from "../components/common/MyButton";
+import ParkingLotDonut from "../components/donut-charts/ParkingLotDonut";
 
 function HomePage() {
   const navigate = useNavigate();
 
   return (
-    <div className="space-y-8 p-4 bg-bg1">
-      <section className="w-full bg-bg2 p-4 rounded-lg">
-        <header className="flex justify-between">
-          <h2 className="text-xl font-bold">Parking Lots Table</h2>
-          <MyButton
-            onClick={() => {
-              navigate("/parking-lots");
-            }}
-          >
-            Open
-          </MyButton>
-        </header>
-        <section className="mt-2">
-          <ParkingTable showPagination={false} />
+    <div className="flex flex-wrap p-2 bg-bg1">
+      <section className="p-2 w-1/3">
+        <section className="w-full bg-bg2 p-4 rounded-lg">
+          <header className="flex justify-between">
+            <h2 className="text-xl font-bold">Parking Lots Verified</h2>
+            <MyButton
+              onClick={() => {
+                navigate("/parking-lots");
+              }}
+            >
+              Open
+            </MyButton>
+          </header>
+
+          <section>
+            <ParkingLotDonut />
+          </section>
         </section>
       </section>
 
-      <section className="w-full bg-bg2 p-4 rounded-lg">
-        <header className="flex justify-between">
-          <h2 className="text-xl font-bold">Users Table</h2>
-          <MyButton
-            onClick={() => {
-              navigate("/users");
-            }}
-          >
-            Open
-          </MyButton>
-        </header>
-        <section className="mt-2">
-          <UsersTable showPagination={false} />
+      <section className="p-2 w-1/3">
+        <section className="w-full bg-bg2 p-4 rounded-lg">
+          <header className="flex justify-between">
+            <h2 className="text-xl font-bold">Users Verified</h2>
+            <MyButton
+              onClick={() => {
+                navigate("/parking-lots");
+              }}
+            >
+              Open
+            </MyButton>
+          </header>
+
+          <section>
+            <ParkingLotDonut />
+          </section>
+        </section>
+      </section>
+
+      <section className="p-2 w-1/3">
+        <section className="w-full bg-bg2 p-4 rounded-lg">
+          <header className="flex justify-between">
+            <h2 className="text-xl font-bold">Vehicles Verified</h2>
+            <MyButton
+              onClick={() => {
+                navigate("/parking-lots");
+              }}
+            >
+              Open
+            </MyButton>
+          </header>
+
+          <section>
+            <ParkingLotDonut />
+          </section>
+        </section>
+      </section>
+
+      {/* <section className="p-2 w-full">
+        <section className="w-full bg-bg2 p-4 rounded-lg">
+          <header className="flex justify-between">
+            <h2 className="text-xl font-bold">Parking Lots Table</h2>
+            <MyButton
+              onClick={() => {
+                navigate("/parking-lots");
+              }}
+            >
+              Open
+            </MyButton>
+          </header>
+          <section className="mt-2">
+            <ParkingTable showPagination={false} />
+          </section>
+        </section>
+      </section> */}
+
+      <section className="p-2 w-full">
+        <section className="w-full bg-bg2 p-4 rounded-lg">
+          <header className="flex justify-between">
+            <h2 className="text-xl font-bold">Users Table</h2>
+            <MyButton
+              onClick={() => {
+                navigate("/users");
+              }}
+            >
+              Open
+            </MyButton>
+          </header>
+          <section className="mt-2">
+            <UsersTable showPagination={false} />
+          </section>
         </section>
       </section>
     </div>
