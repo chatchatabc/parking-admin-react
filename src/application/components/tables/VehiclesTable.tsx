231a5a4dc79627e7e2234ea9aaabd252b6d2cca6
BoJoNVi <bonjomontes@gmail.com>
2023-06-06 16:37:32 +0800

    feat: implemented vehicles table

---
+++ src/application/components/tables/VehiclesTable.tsx
@@ -0,0 +1,44 @@
+import DynamicTable from "./DynamicTable";
+import { vehicleGetAll } from "../../../domain/services/vehicleService";
+import { Vehicle } from "../../../domain/models/VehicleModel";
+import { ColumnsType } from "antd/es/table";
+
+type Props = {
+  showPagination?: boolean;
+};
+
+function VehiclesTable({ showPagination }: Props) {
+  const columns: ColumnsType<Vehicle> = [
+    {
+      title: "Name",
+      key: "name",
+      dataIndex: "name",
+    },
+    {
+      title: "Vehicle Type",
+      key: "type",
+      render: (record: Vehicle) => {
+        if (record.type === 1) {
+          return <p>Motorcycle</p>;
+        }
+        return <p>Car</p>;
+      },
+    },
+    {
+      title: "Plate Number",
+      key: "plateNumber",
+      dataIndex: "plateNumber",
+    },
+  ];
+
+  return (
+    <DynamicTable
+      showPagination={showPagination}
+      columns={columns}
+      title="vehicle-table"
+      getData={vehicleGetAll}
+    />
+  );
+}
+
+export default VehiclesTable;
