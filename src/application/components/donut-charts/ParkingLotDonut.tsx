b62000cce8dc8da6c884e1cb3f36de4dcf18285f
BoJoNVi <bonjomontes@gmail.com>
2023-06-06 13:44:33 +0800

    feat: implemented parking lots chart

---
+++ src/application/components/donut-charts/ParkingLotDonut.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { parkingLotGetDonut } from "../../../domain/services/parkingService";
+import { message } from "antd";
+import DynamicDonut from "./DynamicDonut";
+
+function ParkingLotDonut() {
+  const [loading, setLoading] = React.useState(true);
+  const [series, setSeries] = React.useState<number[]>([]);
+  const [labels, setLabels] = React.useState<string[]>([]);
+
+  React.useEffect(() => {
+    async function fetchData() {
+      const response = await parkingLotGetDonut();
+
+      if (response.errors) {
+        if (response.errors.length > 0) {
+          message.error("Parking lot donut failed");
+        }
+      } else {
+        const data = response.data;
+
+        message.success("Parking lot donut successfully");
+
+        setSeries(data.series);
+        setLabels(data.labels);
+        setLoading(false);
+      }
+    }
+
+    if (loading) {
+      fetchData();
+    }
+  }, [loading]);
+
+  return <DynamicDonut series={series} labels={labels} loading={loading} />;
+}
+
+export default ParkingLotDonut;
