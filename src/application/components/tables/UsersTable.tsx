8f9879ff3980d67b84eb0c2736c39a1339021fd8
BoJoNVi <bonjomontes@gmail.com>
2023-06-07 14:08:13 +0800

    refactor: removed unnecessary words in users table

--- src/application/components/tables/UsersTable.tsx
+++ src/application/components/tables/UsersTable.tsx
@@ -2,6 +2,7 @@ import { User } from "../../../domain/models/UserModel";
 import { useNavigate } from "react-router-dom";
 import DynamicTable from "./DynamicTable";
 import { userGetAll } from "../../../domain/services/userService";
+import { Popover } from "antd";
 
 type Props = {
   showPagination?: boolean;
@@ -38,43 +39,29 @@ function UsersTable({ showPagination, localPagination }: Props) {
       },
     },
     {
-      title: "Status",
-      key: "status",
-      render: (record: User) => {
-        if (record.phoneVerifiedAt || record.emailVerifiedAt) {
-          return <div className="text-green-500 font-bold">Enabled</div>;
-        }
-        return <div className="text-red-500">Disabled</div>;
-      },
-    },
-    {
       title: "Phone",
       key: "phone",
       render: (record: User) => {
         if (record.phone && record.phoneVerifiedAt) {
-          const dateVerified = new Date(record.phoneVerifiedAt);
-          const dateFormatted = dateVerified.toLocaleDateString("en-US", {
-            year: "numeric",
-            month: "long",
-            day: "numeric",
-            hour: "numeric",
-            minute: "numeric",
-          });
           return (
             <div className="flex gap-2 items-center">
-              <span>{record.phone}</span>
-              <span className="text-green-500 text-xs font-bold">
-                ({dateFormatted})
-              </span>
+              <Popover
+                color="green"
+                content={<p className="text-white">Verified</p>}
+              >
+                <span className="text-green-500">{record.phone}</span>
+              </Popover>
             </div>
           );
         } else if (record.phone) {
           return (
             <div className="flex items-center gap-2">
-              <span>{record.phone}</span>
-              <span className="text-red-500 text-xs font-bold">
-                (Not yet verified)
-              </span>
+              <Popover
+                color="red"
+                content={<p className="text-white">Not yet verified</p>}
+              >
+                <span className="text-red-500">{record.phone}</span>
+              </Popover>
             </div>
           );
         }
@@ -86,29 +73,25 @@ function UsersTable({ showPagination, localPagination }: Props) {
       key: "email",
       render: (record: User) => {
         if (record.email && record.emailVerifiedAt) {
-          const dateVerified = new Date(record.emailVerifiedAt);
-          const dateFormatted = dateVerified.toLocaleDateString("en-US", {
-            year: "numeric",
-            month: "long",
-            day: "numeric",
-            hour: "numeric",
-            minute: "numeric",
-          });
           return (
             <div className="flex gap-2 items-center">
-              <span>{record.email}</span>
-              <span className="text-green-500 text-xs font-bold">
-                ({dateFormatted})
-              </span>
+              <Popover
+                color="green"
+                content={<p className="text-white">Verified</p>}
+              >
+                <span className="text-green-500">{record.email}</span>
+              </Popover>
             </div>
           );
         } else if (record.email) {
           return (
             <div className="flex items-center gap-2">
-              <span>{record.email}</span>
-              <span className="text-red-500 text-xs font-bold">
-                (Not yet verified)
-              </span>
+              <Popover
+                color="red"
+                content={<p className="text-white">Not yet verified</p>}
+              >
+                <span className="text-red-500">{record.email}</span>
+              </Popover>
             </div>
           );
         }
