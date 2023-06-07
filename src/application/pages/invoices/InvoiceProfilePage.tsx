cc7c9a0ec396dacbec7496cde8d0b04ee638c134
BoJoNVi <bonjomontes@gmail.com>
2023-06-07 09:30:58 +0800

    chore: added mock profile page for vehicles and invoices

---
+++ src/application/pages/invoices/InvoiceProfilePage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function InvoiceProfilePage() {
+  return <div>InvoiceProfilePage</div>;
+}
+
+export default InvoiceProfilePage;
---
+++ src/application/pages/vehicles/VehiclesProfilePage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function VehiclesProfilePage() {
+  return <div>VehiclesProfilePage</div>;
+}
+
+export default VehiclesProfilePage;
