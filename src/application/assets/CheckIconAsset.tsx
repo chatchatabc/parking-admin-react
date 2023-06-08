42b28035ac62e05d53de2693963fd2bfbfc0eacb
BoJoNVi <bonjomontes@gmail.com>
2023-06-08 15:57:17 +0800

    chore: added check and x icon assets

---
+++ src/application/assets/CheckIconAsset.tsx
@@ -0,0 +1,16 @@
+function CheckIconAsset() {
+  return (
+    <svg
+      className="w-full h-full"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="currentColor"
+        d="m9.55 18l-5.7-5.7l1.425-1.425L9.55 15.15l9.175-9.175L20.15 7.4L9.55 18Z"
+      />
+    </svg>
+  );
+}
+
+export default CheckIconAsset;
