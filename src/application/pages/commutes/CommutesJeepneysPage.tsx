438a3dbde6b17103f9b2f0d874db7411f27c9faa
BoJoNVi <bonjomontes@gmail.com>
2023-06-21 11:53:18 +0800

    feat: implemented jeepneys page

--- src/application/pages/commutes/CommutesJeepneysPage.tsx
+++ src/application/pages/commutes/CommutesJeepneysPage.tsx
@@ -1,10 +1,38 @@
-import React from "react";
+import { useDispatch } from "react-redux";
+import MyButton from "../../components/common/MyButton";
+import { drawerFormUpdate } from "../../redux/slices/drawers/drawerForm";
+import JeepneysTable from "../../components/tables/JeepneysTable";
 
 function CommutesJeepneysPage() {
+  const dispatch = useDispatch();
   return (
     <div className="flex flex-wrap p-2 bg-bg1">
       <div className="p-2 w-full">
-        <div className="bg-bg2 rounded-lg p-4">CommutesJeepneysPage</div>
+        <div className="bg-bg2 rounded-lg p-4">
+          {/* Table Title */}
+          <header className="flex justify-between">
+            <h2 className="text-xl font-bold">Jeepneys</h2>
+
+            <MyButton
+              onClick={() => {
+                dispatch(
+                  drawerFormUpdate({
+                    show: true,
+                    title: "Add Jeepney",
+                    content: "jeepneyCreate",
+                    mode: "create",
+                  })
+                );
+              }}
+            >
+              Add Jeepney +
+            </MyButton>
+          </header>
+
+          <section className="mt-2">
+            <JeepneysTable extraVariables={{}} />
+          </section>
+        </div>
       </div>
     </div>
   );
