36b071f8eb49f3b793d2fb52ca15e769c9690504
BoJoNVi <bonjomontes@gmail.com>
2023-06-21 11:52:37 +0800

    feat: implemented service for getting jeepneys

---
+++ src/domain/gql-docs/jeepneyDocs.ts
@@ -0,0 +1,23 @@
+export function jeepneyGetAllDoc() {
+  return `
+  query GetJeepneys($page: Int = 0, $size: Int = 10, $keyword: String, $sortField: String, $sortBy: Int) {
+    getJeepneys(page: $page, size: $size, keyword: $keyword, sortField: $sortField, sortBy: $sortBy) {
+      content {
+        jeepneyUuid
+        name
+        plateNumber
+        capacity
+        status
+        createdAt
+      }
+      pageInfo {
+        size
+        totalElements
+        first
+        last
+        empty
+      }
+    }
+  }
+  `;
+}
---
+++ src/domain/services/jeepneyService.ts
@@ -0,0 +1,21 @@
+import { jeepneyGetAllDoc } from "../gql-docs/jeepneyDocs";
+import { graphqlQuery } from "../infra/apis/graphqlActions";
+import { AxiosResponseData, AxiosResponseError } from "../models/AxiosModel";
+import { CommonContent, CommonVariables } from "../models/CommonModel";
+import { Jeepney } from "../models/JeepneyModel";
+
+export async function jeepneyGetAll(variables: CommonVariables) {
+  const response = await graphqlQuery(
+    jeepneyGetAllDoc(),
+    variables,
+    "JeepneyGetAll"
+  );
+
+  if (response.data.errors) {
+    return response.data as AxiosResponseError;
+  }
+
+  const data = response.data.data.getJeepneys;
+
+  return { data } as AxiosResponseData<CommonContent<Jeepney>>;
+}
