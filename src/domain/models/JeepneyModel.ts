a959a97c514a64926558e78cc78eddc8ea3ae425
BoJoNVi <bonjomontes@gmail.com>
2023-06-21 11:52:05 +0800

    feat: implemented jeepney model

---
+++ src/domain/models/JeepneyModel.ts
@@ -0,0 +1,8 @@
+export type Jeepney = {
+  jeepneyUuid?: string | null;
+  name?: string | null;
+  plateNumber?: string | null;
+  capacity?: number | null;
+  createdAt?: string | null;
+  status?: number;
+};
