863876aabe9768805c2d7ab24c20108e198349a2
BoJoNVi <bonjomontes@gmail.com>
2023-06-21 12:53:09 +0800

    feat: implemented service for jeepney create

--- src/domain/services/jeepneyService.ts
+++ src/domain/services/jeepneyService.ts
@@ -1,5 +1,6 @@
 import { jeepneyGetAllDoc } from "../gql-docs/jeepneyDocs";
 import { graphqlQuery } from "../infra/apis/graphqlActions";
+import { restPost } from "../infra/apis/restActions";
 import { AxiosResponseData, AxiosResponseError } from "../models/AxiosModel";
 import { CommonContent, CommonVariables } from "../models/CommonModel";
 import { Jeepney } from "../models/JeepneyModel";
@@ -18,4 +19,24 @@ export async function jeepneyGetAll(variables: CommonVariables) {
   const data = response.data.data.getJeepneys;
 
   return { data } as AxiosResponseData<CommonContent<Jeepney>>;
+}
+
+export async function jeepneyCreate(params: Record<string, any>) {
+  params = {
+    name: params.name,
+    plateNumber: params.plateNumber,
+    capacity: params.capacity,
+    latitude: params.latitude,
+    longitude: params.longitude,
+    status: params.status,
+    flag: 0,
+  };
+
+  const response = await restPost("/jeepney", params);
+
+  if (response.data.errors) {
+    return response.data as AxiosResponseError;
+  }
+
+  return response.data as AxiosResponseData<Jeepney>;
 }
