f4b14308dfe0969278a90697dd2983681d7541b0
BoJoNVi <bonjomontes@gmail.com>
2023-06-23 14:26:46 +0800

    feat: implemented commutes nodes page

--- src/application/pages/commutes/CommutesNodesPage.tsx
+++ src/application/pages/commutes/CommutesNodesPage.tsx
@@ -1,32 +1,17 @@
-import React from "react";
-import { RouteNode } from "../../../domain/models/RouteModel";
-import { routeGetNodes } from "../../../domain/services/routeService";
-import { message } from "antd";
+import CommutesNodesMap from "../../components/commutes/CommutesNodesMap";
 
 function CommutesNodesPage() {
-  const [loading, setLoading] = React.useState(true);
-  const [nodes, setNodes] = React.useState<RouteNode[]>([]);
-
-  React.useEffect(() => {
-    if (loading) {
-      (async () => {
-        const response = await routeGetNodes({ size: 10000000, page: 0 });
-
-        if (response.errors) {
-          response.errors.forEach((error) => {
-            message.error(error.message);
-          });
-        } else {
-          console.log(response.data.content);
-          setNodes(response.data.content);
-        }
-
-        setLoading(false);
-      })();
-    }
-  }, []);
-
-  return <div>CommitesNodesPage</div>;
+  return (
+    <div className="flex flex-wrap p-2 bg-bg1">
+      <div className="p-2 w-full">
+        <div className="bg-bg2 rounded-lg p-4">
+          <section>
+            <CommutesNodesMap />
+          </section>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CommutesNodesPage;
