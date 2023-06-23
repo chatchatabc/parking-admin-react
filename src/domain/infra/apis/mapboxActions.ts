d6f76cca0cc96102385aa0f7b5e6e015d0cec4f7
BoJoNVi <bonjomontes@gmail.com>
2023-06-23 09:29:49 +0800

    fix: mapbox can't read token within params

--- src/domain/infra/apis/mapboxActions.ts
+++ src/domain/infra/apis/mapboxActions.ts
@@ -1,15 +1,16 @@
 import { axiosGet } from "../axios/axiosActions";
 
 const baseUrl = "https://api.mapbox.com/matching/v5/mapbox";
-const access_token = import.meta.env.VITE_MAPBOX_TOKEN as string;
+
+export function mapboxGetPublicToken() {
+  return import.meta.env.VITE_MAPBOX_TOKEN;
+}
 
 export async function mapboxGet(
   url: string,
   params: Record<string, any> = {},
   title: string = "MapBox"
 ) {
-  params.access_token = access_token;
-
   const response = await axiosGet(`${baseUrl}${url}`, params, title);
   return response;
 }
