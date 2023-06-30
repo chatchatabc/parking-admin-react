7ff28c23f3742230ff7d254110df2ec0b8c3df2a
Raphile14 <rllddalangin@gmail.com>
2023-06-30 13:19:41 +0800

    feat: added verified status of vehicles on VehicleTable.tsx

--- src/application/components/tables/VehiclesTable.tsx
+++ src/application/components/tables/VehiclesTable.tsx
@@ -2,6 +2,9 @@ import DynamicTable from "./DynamicTable";
 import { vehicleGetAll } from "../../../domain/services/vehicleService";
 import { Vehicle } from "../../../domain/models/VehicleModel";
 import { ColumnsType } from "antd/es/table";
+import { Popover } from "antd";
+import CheckIconAsset from "../../assets/CheckIconAsset";
+import XIconAsset from "../../assets/XIconAsset";
 
 type Props = {
   showPagination?: boolean;
@@ -13,7 +16,43 @@ function VehiclesTable({ showPagination, localPagination }: Props) {
     {
       title: "Name",
       key: "name",
-      dataIndex: "name",
+      render: (record: Vehicle) => {
+        const date = new Date(record.verifiedAt ?? "");
+
+        return (
+          <div className='flex items-center gap-1'>
+            <p>{record.name}</p>
+            <Popover
+              color={record.verifiedAt ? "green" : "red"}
+              content={
+                <p className='text-white text-xs'>
+                  {record.verifiedAt ? (
+                    <span>
+                      Verified at <br />
+                      {new Intl.DateTimeFormat("en", {
+                        dateStyle: "medium",
+                        timeStyle: "short",
+                      }).format(date)}
+                    </span>
+                  ) : (
+                    "Not yet verified"
+                  )}
+                </p>
+              }
+            >
+              {record.verifiedAt ? (
+                <div className='w-5 h-5 text-green-500 self-start'>
+                  <CheckIconAsset />
+                </div>
+              ) : (
+                <div className='w-5 h-5 text-red-500 self-start'>
+                  <XIconAsset />
+                </div>
+              )}
+            </Popover>
+          </div>
+        );
+      },
     },
     {
       title: "Vehicle Type",
@@ -37,7 +76,7 @@ function VehiclesTable({ showPagination, localPagination }: Props) {
       showPagination={showPagination}
       localPagination={localPagination}
       columns={columns}
-      title="vehicle-table"
+      title='vehicle-table'
       getData={vehicleGetAll}
     />
   );
