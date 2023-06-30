f68e56931e38f9b6061ceb44603db3032e4dab5a
Raphile14 <rllddalangin@gmail.com>
2023-06-30 13:19:17 +0800

    improvement: added more fields to VehicleModel.tsx

--- src/domain/models/VehicleModel.ts
+++ src/domain/models/VehicleModel.ts
@@ -2,8 +2,15 @@ export type Vehicle = {
   vehicleUuid?: string | null;
   name?: string | null;
   plateNumber?: string | null;
+  brandUuid?: string | null;
+  modelUuid?: string | null;
+  typeUuid?: string | null;
+  color?: string | null;
+  year?: string | null;
+  verifiedAt?: string | null;
+  rejectionReason?: string | null;
+  status?: number | null;
+  verifiedBy?: number | null;
   createdAt?: string | null;
   updatedAt?: string | null;
-
-  typeUuid?: string | null;
 };
