9146983cc229866bbe7f3bd6c5c215c1c118a3e1
BoJoNVi <bonjomontes@gmail.com>
2023-07-04 14:45:02 +0800

    feat: added vehicle model form to the dynamic drawer

--- src/application/components/forms/VehicleModelForm.tsx
+++ src/application/components/forms/VehicleModelForm.tsx
@@ -51,22 +51,62 @@ function VehicleModelForm({ formRef, title, handleSubmit, loading }: Props) {
       <div className="flex flex-wrap [&>*]:px-2">
         <Form.Item name="modelUuid" hidden></Form.Item>
 
-        <Form.Item className="w-1/2" name="name" label="Name">
+        <Form.Item
+          className="w-1/2"
+          name="name"
+          label="Name"
+          rules={[
+            {
+              message: "Need some input",
+              required: true,
+            },
+          ]}
+        >
           <Input placeholder="Name" />
         </Form.Item>
 
-        <Form.Item className="w-1/2" name="status" label="Status">
+        <Form.Item
+          className="w-1/2"
+          name="status"
+          label="Status"
+          rules={[
+            {
+              message: "Need some input",
+              required: true,
+            },
+          ]}
+        >
           <Select placeholder="Status" options={vehicleOptionsModelStatus()} />
         </Form.Item>
 
-        <Form.Item className="w-1/2" name="brandUuid" label="Brand">
+        <Form.Item
+          className="w-1/2"
+          name="brandUuid"
+          label="Brand"
+          rules={[
+            {
+              message: "Need some input",
+              required: true,
+            },
+          ]}
+        >
           {SelectAsync({
             placeholder: "Brands",
             getData: vehicleOptionsBrandUuid,
           })}
         </Form.Item>
 
-        <Form.Item className="w-1/2" name="typeUuid" label="Type">
+        <Form.Item
+          className="w-1/2"
+          name="typeUuid"
+          label="Type"
+          rules={[
+            {
+              message: "Need some input",
+              required: true,
+            },
+          ]}
+        >
           {SelectAsync({
             placeholder: "Types",
             getData: vehicleOptionsTypeUuid,
