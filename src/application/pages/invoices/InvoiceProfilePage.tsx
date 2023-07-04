c8e971b1fe58fdd5846ea8618c06807007ba94d2
BoJoNVi <bonjomontes@gmail.com>
2023-07-04 13:18:29 +0800

    feat: finalized the invoice profile page

--- src/application/pages/invoices/InvoiceProfilePage.tsx
+++ src/application/pages/invoices/InvoiceProfilePage.tsx
@@ -88,6 +88,16 @@ function InvoiceProfilePage() {
 
           <section className="mt-2 flex flex-wrap gap-y-2">
             <div className="w-1/5">
+              <p className="text-xs font-bold">Total Payment</p>
+              <p>{moneyFormatter.format(invoice?.total ?? 0)}</p>
+            </div>
+
+            <div className="w-1/5">
+              <p className="text-xs font-bold">Hours Taken</p>
+              <p>{invoice?.estimatedParkingDurationInHours} Hours</p>
+            </div>
+
+            <div className="w-1/5">
               <p className="text-xs font-bold">Start At</p>
               <p>{dateFormatter.format(startAtDate)}</p>
             </div>
@@ -98,26 +108,92 @@ function InvoiceProfilePage() {
             </div>
 
             <div className="w-1/5">
-              <p className="text-xs font-bold">Hours Taken</p>
-              <p>{invoice?.estimatedParkingDurationInHours} Hours</p>
-            </div>
-
-            <div className="w-1/5">
               <p className="text-xs font-bold">Paid At</p>
               <p>{dateFormatter.format(paidAtDate)}</p>
-            </div>
-
-            <div className="w-1/5">
-              <p className="text-xs font-bold">Total Payment</p>
-              <p>{moneyFormatter.format(invoice?.total ?? 0)}</p>
             </div>
           </section>
         </section>
       </section>
 
+      {/* Customer Information */}
+      <section className="p-2 w-1/2 flex">
+        <section className="bg-bg2 p-4 rounded-lg w-full">
+          <section>
+            <header className="flex justify-between items-center">
+              <h2 className="text-lg font-bold">Customer Information</h2>
+              <MyButton
+                onClick={() => {
+                  navigate("/users/" + invoice?.parkingLotUuid);
+                }}
+              >
+                View
+              </MyButton>
+            </header>
+
+            <section className="mt-2 flex flex-wrap gap-y-2">
+              <div className="w-1/3">
+                <p className="text-xs font-bold">First Name</p>
+                <p>{invoice?.vehicle?.owner?.firstName ?? "No First Name"}</p>
+              </div>
+
+              <div className="w-1/3">
+                <p className="text-xs font-bold">Last Name</p>
+                <p>{invoice?.vehicle?.owner?.firstName ?? "No Last Name"}</p>
+              </div>
+
+              <div className="w-1/3">
+                <p className="text-xs font-bold">Phone</p>
+                <p>{invoice?.vehicle?.owner?.phone ?? "No Phone"}</p>
+              </div>
+
+              <div className="w-full">
+                <p className="text-xs font-bold">Email</p>
+                <p>{invoice?.vehicle?.owner?.email ?? "No Email"}</p>
+              </div>
+            </section>
+          </section>
+          <section className="mt-8">
+            <header className="flex justify-between items-center">
+              <h2 className="text-lg font-bold">Customer Vehicle</h2>
+            </header>
+
+            <section className="mt-2 flex flex-wrap gap-y-2">
+              <div className="w-1/3">
+                <p className="text-xs font-bold">Plate Number</p>
+                <p>
+                  {invoice?.vehicle?.plateNumber ??
+                    invoice?.plateNumber ??
+                    "Unknown Plate Number"}
+                </p>
+              </div>
+
+              <div className="w-1/3">
+                <p className="text-xs font-bold">Vehicle Color</p>
+                <p>{invoice.vehicle?.color ?? "Unknown Color"}</p>
+              </div>
+
+              <div className="w-1/3">
+                <p className="text-xs font-bold">Vehicle Brand</p>
+                <p>{invoice.vehicle?.model?.brand?.name ?? "Unknown Brand"}</p>
+              </div>
+
+              <div className="w-1/3">
+                <p className="text-xs font-bold">Vehicle Model</p>
+                <p>{invoice.vehicle?.model?.brand?.name ?? "Unknown Model"}</p>
+              </div>
+
+              <div className="w-1/3">
+                <p className="text-xs font-bold">Vehicle Type</p>
+                <p>{invoice?.vehicle?.model?.type?.name ?? "Unknown Type"}</p>
+              </div>
+            </section>
+          </section>
+        </section>
+      </section>
+
       {/* Parking Lot Information */}
-      <section className="p-2 w-1/2">
-        <section className="bg-bg2 p-4 rounded-lg">
+      <section className="p-2 w-1/2 flex">
+        <section className="bg-bg2 p-4 rounded-lg w-full">
           <section>
             <header className="flex justify-between items-center">
               <h2 className="text-lg font-bold">Parking Lot Information</h2>
@@ -178,59 +254,6 @@ function InvoiceProfilePage() {
                   {moneyFormatter.format(invoice?.parkingLot?.rate?.rate ?? 0)}{" "}
                   / {invoice?.parkingLot?.rate?.interval}
                 </p>
-              </div>
-            </section>
-          </section>
-
-          <section className="mt-8">
-            <header className="flex justify-between items-center">
-              <h2 className="text-lg font-bold">Parking Lot Map</h2>
-            </header>
-            <section className="mt-2">
-              <div className="pb-[40%] border rounded-lg"></div>
-            </section>
-          </section>
-        </section>
-      </section>
-
-      {/* Customer Information */}
-      <section className="p-2 w-1/2">
-        <section className="bg-bg2 p-4 rounded-lg">
-          <section>
-            <header className="flex justify-between items-center">
-              <h2 className="text-lg font-bold">Customer Information</h2>
-              <MyButton
-                onClick={() => {
-                  navigate("/users/" + invoice?.parkingLotUuid);
-                }}
-              >
-                View
-              </MyButton>
-            </header>
-
-            <section className="mt-2 flex flex-wrap gap-y-2">
-              <div className="w-1/3">
-                <p className="text-xs font-bold">Plate Number</p>
-                <p>
-                  {invoice?.vehicle?.plateNumber ??
-                    invoice?.plateNumber ??
-                    "Unknown"}
-                </p>
-              </div>
-
-              <div className="w-1/3">
-                <p className="text-xs font-bold">Vehicle Type</p>
-                <p>{invoice?.vehicle?.type?.name}</p>
-              </div>
-
-              <div className="w-1/3">
-                <p className="text-xs font-bold">Closing hours</p>
-                <p>{timeFormatter.format(parkingLotEndDate)}</p>
-              </div>
-
-              <div className="w-full">
-                <p className="text-xs font-bold">Address</p>
-                <p>{invoice?.parkingLot?.address}</p>
               </div>
             </section>
           </section>
