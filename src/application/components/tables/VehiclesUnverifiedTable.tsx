828b7211604c8f125fe0868b5b3b2b3698c12475
BoJoNVi <bonjomontes@gmail.com>
2023-07-05 16:37:32 +0800

    feat: added redirection to vehicle profile from unverified vehicle table

--- src/application/components/tables/VehiclesUnverifiedTable.tsx
+++ src/application/components/tables/VehiclesUnverifiedTable.tsx
@@ -2,6 +2,7 @@ import DynamicTable from "./DynamicTable";
 import { vehicleGetAll } from "../../../domain/services/vehicleService";
 import { Vehicle } from "../../../domain/models/VehicleModel";
 import { ColumnsType } from "antd/es/table";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   showPagination?: boolean;
@@ -9,12 +10,25 @@ type Props = {
 };
 
 function VehiclesUnverifiedTable({ showPagination, localPagination }: Props) {
+  const navigate = useNavigate();
+
   const columns: ColumnsType<Vehicle> = [
     {
       width: "50%",
       title: "Plate Number",
       key: "plateNumber",
-      dataIndex: "plateNumber",
+      render: (record: Vehicle) => {
+        return (
+          <button
+            onClick={() => {
+              navigate("/vehicles/" + record.plateNumber);
+            }}
+            className="underline hover:no-underline"
+          >
+            {record.plateNumber}
+          </button>
+        );
+      },
     },
     {
       width: "50%",
