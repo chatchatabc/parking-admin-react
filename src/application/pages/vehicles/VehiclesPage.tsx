2a182a05558156ba978e69ed46052a3f563afe14
BoJoNVi <bonjomontes@gmail.com>
2023-07-06 13:37:17 +0800

    fix: other tables aside from vehicle are showing when using global search

--- src/application/pages/vehicles/VehiclesPage.tsx
+++ src/application/pages/vehicles/VehiclesPage.tsx
@@ -5,9 +5,13 @@ import { drawerFormUpdate } from "../../redux/slices/drawers/drawerForm";
 import VehicleModelTable from "../../components/tables/VehicleModelTable";
 import VehicleTypeTable from "../../components/tables/VehicleTypeTable";
 import VehicleBrandTable from "../../components/tables/VehicleBrandTable";
+import { useSearchParams } from "react-router-dom";
 
 function VehiclesPage() {
   const dispatch = useDispatch();
+
+  const [searchParams, _] = useSearchParams();
+  const keyword = searchParams.get("keyword");
 
   return (
     <div className="p-4 space-y-4 bg-bg1 w-full relative">
@@ -38,84 +42,88 @@ function VehiclesPage() {
         </section>
       </section>
 
-      {/* Vehicle Models Table */}
-      <section className="bg-bg2 p-4 space-y-2 rounded-lg w-full">
-        {/* Table Title */}
-        <header className="flex justify-between">
-          <h2 className="text-xl font-bold">Vehicle Models</h2>
-          <MyButton
-            onClick={() => {
-              dispatch(
-                drawerFormUpdate({
-                  show: true,
-                  title: "Add Model",
-                  content: "vehicleModel",
-                  mode: "create",
-                })
-              );
-            }}
-          >
-            Add Vehicle Model +
-          </MyButton>
-        </header>
+      {!keyword && (
+        <>
+          {/* Vehicle Models Table */}
+          <section className="bg-bg2 p-4 space-y-2 rounded-lg w-full">
+            {/* Table Title */}
+            <header className="flex justify-between">
+              <h2 className="text-xl font-bold">Vehicle Models</h2>
+              <MyButton
+                onClick={() => {
+                  dispatch(
+                    drawerFormUpdate({
+                      show: true,
+                      title: "Add Model",
+                      content: "vehicleModel",
+                      mode: "create",
+                    })
+                  );
+                }}
+              >
+                Add Vehicle Model +
+              </MyButton>
+            </header>
 
-        <section>
-          <VehicleModelTable />
-        </section>
-      </section>
+            <section>
+              <VehicleModelTable />
+            </section>
+          </section>
 
-      {/* Vehicle Brands */}
-      <section className="bg-bg2 p-4 space-y-2 rounded-lg w-full">
-        {/* Table Title */}
-        <header className="flex justify-between">
-          <h2 className="text-xl font-bold">Vehicle Brands</h2>
-          <MyButton
-            onClick={() => {
-              dispatch(
-                drawerFormUpdate({
-                  show: true,
-                  title: "Add Vehicle Brand",
-                  content: "vehicleBrand",
-                  mode: "create",
-                })
-              );
-            }}
-          >
-            Add Vehicle Brand +
-          </MyButton>
-        </header>
+          {/* Vehicle Brands */}
+          <section className="bg-bg2 p-4 space-y-2 rounded-lg w-full">
+            {/* Table Title */}
+            <header className="flex justify-between">
+              <h2 className="text-xl font-bold">Vehicle Brands</h2>
+              <MyButton
+                onClick={() => {
+                  dispatch(
+                    drawerFormUpdate({
+                      show: true,
+                      title: "Add Vehicle Brand",
+                      content: "vehicleBrand",
+                      mode: "create",
+                    })
+                  );
+                }}
+              >
+                Add Vehicle Brand +
+              </MyButton>
+            </header>
 
-        <section>
-          <VehicleBrandTable />
-        </section>
-      </section>
+            <section>
+              <VehicleBrandTable />
+            </section>
+          </section>
 
-      {/* Vehicle Types */}
-      <section className="bg-bg2 p-4 space-y-2 rounded-lg w-full">
-        {/* Table Title */}
-        <header className="flex justify-between">
-          <h2 className="text-xl font-bold">Vehicle Types</h2>
-          <MyButton
-            onClick={() => {
-              dispatch(
-                drawerFormUpdate({
-                  show: true,
-                  title: "Add Type",
-                  content: "vehicleType",
-                  mode: "create",
-                  data: {},
-                })
-              );
-            }}
-          >
-            Add Vehicle Type +
-          </MyButton>
-        </header>
+          {/* Vehicle Types */}
+          <section className="bg-bg2 p-4 space-y-2 rounded-lg w-full">
+            {/* Table Title */}
+            <header className="flex justify-between">
+              <h2 className="text-xl font-bold">Vehicle Types</h2>
+              <MyButton
+                onClick={() => {
+                  dispatch(
+                    drawerFormUpdate({
+                      show: true,
+                      title: "Add Type",
+                      content: "vehicleType",
+                      mode: "create",
+                      data: {},
+                    })
+                  );
+                }}
+              >
+                Add Vehicle Type +
+              </MyButton>
+            </header>
 
-        <section>
-          <VehicleTypeTable />
-        </section>
-      </section>
+            <section>
+              <VehicleTypeTable />
+            </section>
+          </section>
+        </>
+      )}
     </div>
   );
 }
